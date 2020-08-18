f54be4384f21020ae11e6f42549efc42d49fd00f
guilherme.pereira <guilherme.pereira@ebc.com.br>
2020-08-18 02:11:34 -0300

    adicionado background e desafio finalizado

--- desafio_flutter/lib/app/modules/home/home_page.dart
+++ desafio_flutter/lib/app/modules/home/home_page.dart
@@ -12,122 +12,133 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
   //use 'controller' variable to access controller
-
+  String buttonText = "ENTRAR";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
       extendBodyBehindAppBar: true,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Container(
-              alignment: AlignmentDirectional.topStart,
-              decoration: BoxDecoration(
-                color: Colors.yellow[700],
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/pattern_customizada_2.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                alignment: AlignmentDirectional.topStart,
+                decoration: BoxDecoration(
+                  color: Colors.yellow[700],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
                 ),
-              ),
-              height: MediaQuery.of(context).size.height * 0.45,
-              width: MediaQuery.of(context).size.height,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Container(
-                      alignment: AlignmentDirectional.topStart,
-                      child: Text(
-                        'Bem vindo(a)',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.grey[800]),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                    child: Container(
-                      height: 45,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 0.5),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      child: TextField(
-                        obscureText: true,
-                        textAlign: TextAlign.start,
-                        decoration: InputDecoration(
-                          hintText: "Email: ",
-                          contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    child: Container(
-                      height: 45,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 0.5),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      child: TextField(
-                        obscureText: true,
-                        textAlign: TextAlign.start,
-                        decoration: InputDecoration(
-                          hintText: "Senha: ",
-                          contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 15),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 45,
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        disabledColor: Colors.grey[800],
-                        onPressed: null,
+                height: MediaQuery.of(context).size.height * 0.45,
+                width: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Container(
+                        alignment: AlignmentDirectional.topStart,
                         child: Text(
-                          "ENTRAR",
+                          'Bem vindo(a)',
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Colors.white),
+                              fontSize: 18,
+                              color: Colors.grey[800]),
                         ),
                       ),
                     ),
-                  ),
-                  Text(
-                    'ESQUECI A SENHA',
-                    style: TextStyle(
-                        fontSize: 11,
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: Container(
+                        height: 45,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.5),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: TextField(
+                          textAlign: TextAlign.start,
+                          decoration: InputDecoration(
+                            hintText: "Email: ",
+                            contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Container(
+                        height: 45,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.5),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: TextField(
+                          obscureText: true,
+                          textAlign: TextAlign.start,
+                          decoration: InputDecoration(
+                            hintText: "Senha: ",
+                            contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 15),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 45,
+                        child: RaisedButton(
+                          onPressed: () {
+                            setState(() {
+                              print(buttonText);
+                              buttonText = "DESAFIO CONCLUIDO";
+                            });
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          color: Colors.grey[800],
+                          child: Text(
+                            buttonText,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'ESQUECI A SENHA',
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
