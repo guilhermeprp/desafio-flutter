04b83e51399f930aebe2822d9ed3e0d3fa72f3c2
guilherme.pereira <guilherme.pereira@ebc.com.br>
2020-08-18 00:38:37 -0300

    Tela de login desafio Flutter

--- desafio_flutter/lib/app/modules/home/home_page.dart
+++ desafio_flutter/lib/app/modules/home/home_page.dart
@@ -16,13 +16,117 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
+      extendBodyBehindAppBar: true,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Container(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              alignment: AlignmentDirectional.topStart,
+              decoration: BoxDecoration(
+                color: Colors.yellow[700],
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.45,
+              width: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      alignment: AlignmentDirectional.topStart,
+                      child: Text(
+                        'Bem vindo(a)',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.grey[800]),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Container(
+                      height: 45,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.5),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                          hintText: "Email: ",
+                          contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: Container(
+                      height: 45,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.5),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.start,
+                        decoration: InputDecoration(
+                          hintText: "Senha: ",
+                          contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 15),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 45,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        disabledColor: Colors.grey[800],
+                        onPressed: null,
+                        child: Text(
+                          "ENTRAR",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'ESQUECI A SENHA',
+                    style: TextStyle(
+                        fontSize: 11,
+                        color: Colors.grey[800],
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
